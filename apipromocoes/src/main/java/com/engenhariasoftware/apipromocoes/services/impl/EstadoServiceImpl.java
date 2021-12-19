35f400a3e695925a2148635049f2e238c0e359c9
Maycon Santana <maycon_gus2009@hotmail.com>
2021-12-19 19:06:16 -0300

    Dependencia para Model Mapper

--- apipromocoes/src/main/java/com/engenhariasoftware/apipromocoes/services/impl/EstadoServiceImpl.java
+++ apipromocoes/src/main/java/com/engenhariasoftware/apipromocoes/services/impl/EstadoServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.engenhariasoftware.apipromocoes.domain.Estado;
+import com.engenhariasoftware.apipromocoes.domain.dtos.EstadoDTO;
 import com.engenhariasoftware.apipromocoes.repositories.EstadoRepository;
 import com.engenhariasoftware.apipromocoes.services.EstadoService;
 import com.engenhariasoftware.apipromocoes.services.exceptions.ObjectNotFoundException;
@@ -22,9 +23,16 @@ public class EstadoServiceImpl implements EstadoService {
 		Optional<Estado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
+	@Override
 	public List<Estado> findAll() {
 		return repository.findAll();
+	}
+
+	@Override
+	public Estado create(EstadoDTO obj) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
